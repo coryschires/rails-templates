f666275807fb17283d1d83e31399c7ed2a68c222
Cory Schires <coryschires@gmail.com>
2010-04-26 20:52:26 -0500

    Working on sass setup template.

--- setup_sass.rb
+++ setup_sass.rb
@@ -1,2 +1,14 @@
 # require gem
 gem 'haml'
+
+# grab sass templates
+reset = run "curl "
+utilities = run "curl "
+basic = run "curl"
+application = run "curl"
+
+# add them to the sass folder
+file 'public/stylesheets/sass/_reset.sass', "#{reset}"
+file 'public/stylesheets/sass/_utilities.sass', "#{utilities}"
+file 'public/stylesheets/sass/_basic.sass', "#{basic}"
+file 'public/stylesheets/sass/application.sass', "#{application}"
