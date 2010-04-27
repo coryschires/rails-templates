e4be4785a72523b89ac31140206e7ac14806e57d
Cory Schires <coryschires@gmail.com>
2010-04-26 23:51:14 -0500

    templates.

--- setup_javascripts.rb
+++ setup_javascripts.rb
@@ -8,7 +8,7 @@ run "rm public/javascripts/prototype.js"
 rails_js = run "curl http://github.com/rails/jquery-ujs/raw/59dd91d945570391f905b1e40444e5921dbc2b8f/src/rails.js"
 validate_js = run "curl http://ajax.microsoft.com/ajax/jquery.validate/1.7/jquery.validate.js"
 css_browser_selector_js = run "curl http://github.com/rafaelp/css_browser_selector/raw/master/css_browser_selector.js"
-js_browser_selector_js = run "cur http://jquery.thewikies.com/browser/jquery.browser.min.js"
+js_browser_selector_js = run "curl http://jquery.thewikies.com/browser/jquery.browser.min.js"
 application_js = run "curl http://github.com/coryschires/rails-templates/blob/master/app-templates/application.js"
 
 file 'public/javascripts/rails.js', rails_js, :force => true
