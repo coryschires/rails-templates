4f74bf7cc15247af48f1092fc9fceb2063908af3
Cory Schires <coryschires@gmail.com>
2010-04-26 23:48:04 -0500

    templates.

--- setup_admin_section.rb
+++ setup_admin_section.rb
@@ -1,1 +1,14 @@
-generate(:controller, "admin dashboard")
+# create admin controller
+# generate(:controller, "admin dashboard")
+
+# create admin style sheet
+admin_style = run "curl http://github.com/coryschires/rails-templates/raw/master/sass-templates/admin.sass"
+file 'public/stylesheets/sass/admin.sass', admin_style
+
+# create admin javascript
+admin_js = run "curl http://github.com/coryschires/rails-templates/raw/master/app-templates/admin.js"
+file 'public/javascripts/admin.js', admin_js
+
+# create admin layout
+admin_layout = run "curl http://github.com/coryschires/rails-templates/raw/master/app-templates/admin.html.erb"
+file 'app/views/layouts/admin.html.erb', admin_layout
--- setup_application.rb
+++ setup_application.rb
@@ -6,27 +6,30 @@ run 'rm public/images/rails.png'
 run 'touch README'
 
 # setup javascripts
-apply "http://github.com/coryschires/rails-templates/raw/master/setup_jquery.rb"
+apply "http://github.com/coryschires/rails-templates/raw/master/setup_javascripts.rb"
 
 # install stylesheets (using sass)
-apply "http://github.com/coryschires/rails-templates/raw/master/setup_sass.rb"
+apply "http://github.com/coryschires/rails-templates/raw/master/setup_stylesheets.rb"
 
-# add templates
-    # application_controller.rb
-    # layout_helper.rb
-    # application.html.erb    
+# add required templates
+application_controller = run "curl http://github.com/coryschires/rails-templates/raw/master/app-templates/application_controller.rb"
+application_helper = run "curl http://github.com/coryschires/rails-templates/raw/master/app-templates/application_helper.rb"
+application_layout = run "curl http://github.com/coryschires/rails-templates/raw/master/app-templates/application.html.erb"
+
+file 'app/views/layouts/admin.html.erb', application_controller, :force => true
+file 'app/views/layouts/admin.html.erb', application_helper, :force => true
+file 'app/views/layouts/admin.html.erb', application_layout, :force => true
+
     
-
-# setup testing evnironment
-
-
 # create admin section (optional)
+# apply "http://github.com/coryschires/rails-templates/raw/master/setup_admin_section.rb"
     # admin.js
     # admin.sass    
     # admin.html.erb
     # generate admin controller
     # replace dashboard.html.erb
 
+# setup testing evnironment (optional)
 
 # install authentication with devise (optional)
 
