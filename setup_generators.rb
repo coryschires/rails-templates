5eb93179b9cccfc1e046ebe5b463171a14314b83
Cory Schires <coryschires@gmail.com>
2010-06-16 01:27:00 -0500

    ditto

--- setup_generators.rb
+++ setup_generators.rb
@@ -1,13 +1,12 @@
 default_generator_config = /\s+# Configure generators values.+\n.+\n.+\n.+\n.+\n.+/
 
 custom_generator_config = %q{
-config.generators do |g|
-  g.template_engine       :haml
-  g.test_framework        :rspec
-  g.stylesheets           false
-  g.fixture_replacement   :factory_girl
-  g.view_specs            false
-end
-}
+    config.generators do |g|
+      g.template_engine       :haml
+      g.test_framework        :rspec
+      g.stylesheets           false
+      g.fixture_replacement   :factory_girl
+      g.view_specs            false
+    end}
 
 gsub_file 'config/application.rb', /#{default_generator_config}/, custom_generator_config
