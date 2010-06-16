2cb63cd38bc5144cfac712c14e222a9dc56dad71
Cory Schires <coryschires@gmail.com>
2010-06-16 00:47:13 -0500

    ditto

--- setup_generators.rb
+++ setup_generators.rb
@@ -1,9 +1,10 @@
-default_generator_config = %q{# Configure generators values. Many other options are available, be sure to check the documentation.
-# config.generators do |g|
-#   g.orm             :active_record
-#   g.template_engine :erb
-#   g.test_framework  :test_unit, :fixture => true
-# end}
+default_generator_config = %q{
+    # Configure generators values. Many other options are available, be sure to check the documentation.
+    # config.generators do |g|
+    #   g.orm             :active_record
+    #   g.template_engine :erb
+    #   g.test_framework  :test_unit, :fixture => true
+    # end}
 
 # custom_generator_config = %q{
 # config.generators do |g|
